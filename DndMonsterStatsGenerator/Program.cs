68632093d4e433ebf173964e4796715b4d9c145e
Antoine Laroche <antoine_laroche7@hotmail.fr>
2019-08-23 22:03:45 -0400

    Ajoutez des fichiers projet.

---
+++ DndMonsterStatsGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DndMonsterStatsGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
