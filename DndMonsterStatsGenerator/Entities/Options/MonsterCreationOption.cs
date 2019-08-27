6e13e893ffec4baf273ef37f5fd7d9adc7ecb612
Antoine Laroche <antoine_laroche7@hotmail.fr>
2019-08-26 20:36:38 -0400

    removed useless commentary

--- DndMonsterStatsGenerator/Entities/Options/MonsterCreationOption.cs
+++ DndMonsterStatsGenerator/Entities/Options/MonsterCreationOption.cs
@@ -3,9 +3,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DndMonsterStatsGenerator.Entities.Options
 {
-    /// <summary>
-    /// test
-    /// </summary>
     [Verb("create", HelpText = "Create a monster stats according to the cr")]
     public class MonsterCreationOption
     {
