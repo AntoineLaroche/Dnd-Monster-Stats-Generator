da020e9706056ff7d430784cfc6d544c05bb29ef
AntoineL <antoine.laroche@akinox.com>
2020-05-19 17:44:58 -0400

    changed syntax for MonsterStatsGeneratorStrategyFactory switch expression

--- DndMonsterStatsGenerator/Factory/MonsterStatsGenerator/MonsterStatsGeneratorStrategyFactory.cs
+++ DndMonsterStatsGenerator/Factory/MonsterStatsGenerator/MonsterStatsGeneratorStrategyFactory.cs
@@ -8,22 +8,15 @@ namespace DndMonsterStatsGenerator.Factory.MonsterStatsGenerator
     {
         public IMonsterStatsGeneratorStrategy Get(MonsterCreationOption monsterCreationOptions)
         {
-            return monsterCreationOptions switch
+            return monsterCreationOptions.CR switch
             {
-                MonsterCreationOption option when option.CR == 0 => new MonsterWithCRZeroStatsGeneratorStrategy(),
-
-                MonsterCreationOption option when option.CR == 0.125 => new MonsterWithCROneEightStatsGeneratorStrategy(),
-
-                MonsterCreationOption option when option.CR == 0.25 => new MonsterWithCROneQuarterStatsGeneratorStrategy(),
-
-                MonsterCreationOption option when option.CR == 0.5 => new MonsterWithCRHalfStatsGeneratorStrategy(),
-
-                MonsterCreationOption options when options.CR == 1 => new MonsterWithCROneStatsGeneratorStrategy(),
-
-                MonsterCreationOption opts when opts.CR >= 2 && opts.CR < 8 => new MonsterWithCRBetweenTwoAndSevenStatsGeneratorStrategy(),
-
-                MonsterCreationOption monsterCreationOption when monsterCreationOption.CR >= 8 => new MonsterWithCREightOrHigherStatsGeneratorStrategy(),
-
+                0 => new MonsterWithCRZeroStatsGeneratorStrategy(),
+                0.125 => new MonsterWithCROneEightStatsGeneratorStrategy(),
+                0.25 => new MonsterWithCROneQuarterStatsGeneratorStrategy(),
+                0.5 => new MonsterWithCRHalfStatsGeneratorStrategy(),
+                1 => new MonsterWithCROneStatsGeneratorStrategy(),
+                double cr when cr >= 2 && cr < 8 => new MonsterWithCRBetweenTwoAndSevenStatsGeneratorStrategy(),
+                double cr when cr >= 8 => new MonsterWithCREightOrHigherStatsGeneratorStrategy(),
                 _ => throw new NotImplementedException(),
             };
         }
